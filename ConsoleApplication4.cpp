119bcad6b8ee727f263d138e4dab0517a5e204db
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-27 20:33:10 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,40 @@
+﻿//  Чертоусова 27.10.2022 ср1 Слоник
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, i = 0;
+	cin >> a;
+	while (a > 4) {
+		a -= 5;
+		i++;
+	 }
+	if (a > 3)
+	{
+		a -= 4;
+		i++;
+	}
+	if(a > 2) 
+	 {
+		a -= 3; 
+		i++;
+	}
+	if (a>1) {
+	a -= 2;
+	i++;
+
+    }
+	if (a > 0)
+	{
+	a -= 1;
+	i++;
+    }
+	cout << i;
+}
+
+
+
+
+
+
